deb7168f2b32a79ba8f3df04801d8f35e6d64aa8
khoi.tuan <khoi.tuan@ladbrokes.com.au>
2022-04-22 09:54:46 +1000

    modify container/ to trigger GitHub Actions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>You should see the marketing app below</h1>
+      <h1>[Container] You should see the marketing app below 👇</h1>
       <hr />
       <MarketingApp />
     </>
