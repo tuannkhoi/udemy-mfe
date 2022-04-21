083699c84df04362339c4220cfbed558be668d60
khoi.tuan <khoi.tuan@ladbrokes.com.au>
2022-04-22 08:50:43 +1000

    init commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <>
+      <h1>You should see the marketing app below</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
